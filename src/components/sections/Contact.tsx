e74fc3455f710a4939a03f37fd98cea058acbc1e
Vaibhavee Singh <vaibhaveesingh89@gmail.com>
2025-03-08 08:06:25 +0530

    remove GMap

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -109,7 +109,7 @@ export const Contact = () => {
               </div>
             </div>
             
-            <div className="aspect-square max-w-xs mx-auto lg:mx-0 rounded-xl overflow-hidden mt-8">
+            {/* <div className="aspect-square max-w-xs mx-auto lg:mx-0 rounded-xl overflow-hidden mt-8">
               <iframe 
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830869428!2d-74.11976397304903!3d40.69766374874431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sus!4v1656442509630!5m2!1sen!2sus" 
                 width="100%" 
@@ -120,7 +120,7 @@ export const Contact = () => {
                 referrerPolicy="no-referrer-when-downgrade"
                 title="Map location"
               ></iframe>
-            </div>
+            </div> */}
           </div>
           
           <div className={`${isVisible ? 'animate-fade-in animate-delay-300' : 'opacity-0'}`}>
