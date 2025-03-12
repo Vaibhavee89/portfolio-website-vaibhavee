018bcce4fcce2f2f56ee854c4c7f3e6fcc052a80
Vaibhavee Singh <vaibhaveesingh89@gmail.com>
2025-03-12 09:57:23 +0530

    Student volunteer caption update

--- src/components/sections/EventCarousel.tsx
+++ src/components/sections/EventCarousel.tsx
@@ -18,15 +18,9 @@ const eventImages = [
     caption: "AI/ML Conference"
   },
   {
-<<<<<<< HEAD
-    src: "https://drive.google.com/file/d/1qA3P1M9SKjCxPpRalwFQIyoJdgUV_cKY/view?usp=sharing",
-    alt: "Student Volunteer at ICICC- 2025",
-    caption: "Student Volunteer at ICICC- 2025"
-=======
     src: "public\Student_volunteer_conference.jpg",
-    alt: "Hackathon 2023",
-    caption: "Cloud Computing Hackathon"
->>>>>>> e42e10999d71ef240b6170a8ca878a516ad0e402
+    alt: "Student Volunteer at ICICC-2025",
+    caption: "Student Volunteer at ICICC-2025"
   },
   {
     src: "https://images.unsplash.com/photo-1518770660439-4636190af475?auto=format&fit=crop&q=80",
